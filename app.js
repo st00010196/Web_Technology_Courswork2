cc55eeca2be11dab001efc460c57c328f6b4253f
st00010196 <khabibulinismail236@gmail.com>
2021-04-07 18:47:23 -0400

    home page is completed

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(5000, () => console.log("App is running on port 5000..."))
